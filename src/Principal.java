cbdd41433e2d160df7a361cc85abf0cbfda2b62a
Leonardo Alves <104588087+Zhorg10@users.noreply.github.com>
2023-08-25 18:48:58 -0300

    25-08-2023

--- src/Principal.java
+++ src/Principal.java
@@ -1,2 +1,49 @@
-package PACKAGE_NAME;public class Principal {
+import br.com.zhorg.screenmatch.modelos.CalculadoraDeTempo;
+import br.com.zhorg.screenmatch.modelos.Filme;
+import br.com.zhorg.screenmatch.modelos.Serie;
+
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("O poderoso chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setSomaAvaliacoes(8.5);
+        meuFilme.setIncluidoNoPlano(true);
+        meuFilme.setDuracaoEmMinutos(180);
+        meuFilme.avalia(9.5);
+        meuFilme.avalia(9);
+        meuFilme.avalia(8);
+        System.out.println(meuFilme.getSomaAvaliacoes());
+        System.out.println(meuFilme.getTotalDeAvaliacoes());
+        meuFilme.exibeFichaTecnica();
+        System.out.println("\nMédia das avaliações: " + (float) meuFilme.mediaAvaliacoes());
+        System.out.println();
+
+        Filme outroFilme = new Filme();
+        outroFilme.setNome("Outro filme qualquer");
+        outroFilme.setAnoDeLancamento(1980);
+        outroFilme.setSomaAvaliacoes(9);
+        outroFilme.setIncluidoNoPlano(true);
+        outroFilme.setDuracaoEmMinutos(220);
+
+        Serie minhaSerie = new Serie();
+        minhaSerie.setNome("Lost");
+        minhaSerie.setAnoDeLancamento(1990);
+        minhaSerie.setTemporadas(10);
+        minhaSerie.setIncluidoNoPlano(true);
+        minhaSerie.avalia(7);
+        minhaSerie.avalia(6);
+        minhaSerie.avalia(5);
+        minhaSerie.setEpisodiosPorTemporada(8);
+        minhaSerie.setMinutosPorEpisodios(50);
+        minhaSerie.exibeFichaTecnica();
+        System.out.println("\nMédia das avaliações: " + (float) minhaSerie.mediaAvaliacoes());
+        System.out.println();
+
+        CalculadoraDeTempo calc = new CalculadoraDeTempo();
+        calc.inclui(meuFilme);
+        calc.inclui(outroFilme);
+        calc.inclui(minhaSerie);
+        System.out.println("Tempo gasto para assistir tudo: " + calc.getTempoTotal() + " minutos.");
+    }
 }
--- src/br/com/zhorg/screenmatch/modelos/Serie.java
+++ src/br/com/zhorg/screenmatch/modelos/Serie.java
@@ -1,2 +1,50 @@
-package br.com.zhorg.screenmatch.modelos;public class Serie {
+package br.com.zhorg.screenmatch.modelos;
+
+public class Serie extends Titulo{
+    private int temporadas;
+    private boolean ativo;
+    private int episodiosPorTemporada;
+    private int minutosPorEpisodios;
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public int getEpisodiosPorTemporada() {
+        return episodiosPorTemporada;
+    }
+
+    public void setEpisodiosPorTemporada(int episodiosPorTemporada) {
+        this.episodiosPorTemporada = episodiosPorTemporada;
+    }
+
+    public int getMinutosPorEpisodios() {
+        return minutosPorEpisodios;
+    }
+
+    public void setMinutosPorEpisodios(int minutosPorEpisodios) {
+        this.minutosPorEpisodios = minutosPorEpisodios;
+    }
+
+    @Override
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.setDuracaoEmMinutos(duracaoEmMinutos);
+    }
+
+    @Override
+    public int getDuracaoEmMinutos() {
+        return this.temporadas * this.episodiosPorTemporada * this.minutosPorEpisodios;
+    }
 }
