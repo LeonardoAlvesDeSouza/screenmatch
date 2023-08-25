1b0b7889daf06c6e733c6d45c503d10b9052d64e
Leonardo Alves <104588087+Zhorg10@users.noreply.github.com>
2023-08-25 18:15:22 -0300

    Commit inicial

---
+++ src/br/com/zhorg/screenmatch/modelos/Filme.java
@@ -0,0 +1,74 @@
+public class Filme {
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaAvaliacoes;
+    private int totalDeAvaliacoes;
+    private int duracaoEmMinutos;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public double getSomaAvaliacoes() {
+        return somaAvaliacoes;
+    }
+
+    public void setSomaAvaliacoes(double somaAvaliacoes) {
+        this.somaAvaliacoes = somaAvaliacoes;
+    }
+
+    public int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
+    }
+
+    public void setTotalDeAvaliacoes(int totalDeAvaliacoes) {
+        this.totalDeAvaliacoes = totalDeAvaliacoes;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    public void exibeFichaTecnica(){
+        System.out.println("Nome do filme: " + this.nome);
+        System.out.println("Ano de lançamento: " + this.anoDeLancamento);
+        System.out.println("Incluído no plano: " + this.incluidoNoPlano);
+        System.out.println("Soma das avaliações: " + this.somaAvaliacoes);
+        System.out.println("Total de avaliações: " + this.totalDeAvaliacoes);
+        System.out.println("Duração em minutos: " + this.duracaoEmMinutos);
+    }
+
+    public void avalia(double nota){
+        this.somaAvaliacoes += nota;
+        this.totalDeAvaliacoes++;
+    }
+
+    public double mediaAvaliacoes(){
+        return this.somaAvaliacoes / this.totalDeAvaliacoes;
+    }
+}
---
+++ src/br/com/zhorg/screenmatch/modelos/Serie.java
@@ -0,0 +1,2 @@
+package br.com.zhorg.screenmatch.modelos;public class Serie {
+}
